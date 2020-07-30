cbf5817dfacfb33d3d32e758e5bc8d985df18a8d
Jannis R <mail@jannisr.de>
2020-07-30 19:34:04 +0200

    basic test ✅

--- test/index.js
+++ test/index.js
@@ -1,8 +1,17 @@
 'use strict'
 
+const {readFileSync} = require('fs')
+const {join: pJoin} = require('path')
 const test = require('tape')
 const parse = require('..')
+const expected = require('./ne_110m_geography_regions_points')
 
-test('todo', (t) => {
-	// todo
+const naturalEarthRegionsPoints = readFileSync(
+	pJoin(__dirname, 'ne_110m_geography_regions_points.spaten'),
+)
+
+test('parsing works', (t) => {
+	const parsed = Array.from(parse(naturalEarthRegionsPoints))
+	t.deepEqual(parsed, expected)
+	t.end()
 })
